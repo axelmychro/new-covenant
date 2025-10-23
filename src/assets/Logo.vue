7b748b6fe6fd3a4e1fb607ed987d687dd9222128
Axel <maxelramadhan@gmail.com>
2025-10-23 21:43:54 +0700

    logo in place

---
+++ src/assets/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="container"><div id="diamond"></div></div>
+</template>
+
+<style scoped>
+  div {
+    width: 8rem;
+    height: 8rem;
+    aspect-ratio: 1/1;
+    background-color: var(--logo-in);
+  }
+  #container {
+    background-color: var(--logo-in);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 2%;
+  }
+  #diamond {
+    border: 8px solid var(--logo-out);
+    transform: rotate(45deg);
+    background-color: var(--logo-in);
+    width: 50%;
+    height: 50%;
+  }
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Logo from '../assets/Logo.vue'
+
+</script><template>
+  <nav>
+    <div id="logo"><Logo></Logo></div>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    background-color: green;
+    width: 80%;
+    height: 2rem;
+    margin: 10vh;
+  }
+</style>
